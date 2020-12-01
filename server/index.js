d0a658a56dffe84421d4724325d1a8adaba69cec
doi-h <jeonghwanlee15@gmail.com>
2020-12-01 15:23:53 +0900

    test https999

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 const {user} = require('./models');
 const {post} = require('./models');
 const { invention } = require('./models');
