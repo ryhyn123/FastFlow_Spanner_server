0cea4b1d752599f58ee8c99e6a9e98a795e44310
doi-h <jeonghwanlee15@gmail.com>
2020-12-04 10:42:39 +0900

    Update : post/edit editing

--- server/index.js
+++ server/index.js
@@ -205,7 +205,6 @@ app.put('/post/edit', authenticateToken,(req, res) => {
     post.update({
     text: req.body.text, 
     title: req.body.title,
-    postPhoto: req.body.postPhoto
   }, {
     where: { id:req.body.postId, userId:req.user.userId, inventionId:req.body.inventionId }
   })
